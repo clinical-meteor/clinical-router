39e766c15ec35f1e43ab2293913d85fe95bab6a7
Abigail Watson <awatson1978@gmail.com>
2015-12-15 20:58:22 -0800

    RouterTests.js

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'clinical:router',
   summary: 'Routing specifically designed for Meteor',
-  version: '2.0.15',
+  version: '2.0.16',
   git: 'https://github.com/clinical-meteor/clinical-router'
 });
 
